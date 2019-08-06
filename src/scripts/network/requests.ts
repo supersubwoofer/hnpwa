638812818480a0929a40708e4306b5c05a5c0ee6
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2019-08-06 12:51:13 -0700

    remove code and set user-agent

--- src/scripts/network/requests.ts
+++ src/scripts/network/requests.ts
@@ -1,7 +1,12 @@
 import * as rm from 'typed-rest-client/RestClient';
 
+let userAgent = '';
+if ('userAgent' in navigator) {
+  userAgent = navigator.userAgent;
+}
+
 export const getResource = async<T>(baseUrl:string, version:string, resourceUrl:string)
 : Promise<rm.IRestResponse<T>> => {
-  const rest: rm.RestClient = new rm.RestClient('get-resource', baseUrl);
+  const rest: rm.RestClient = new rm.RestClient(userAgent, baseUrl);
   return await rest.get<T>(version + resourceUrl);
 };
