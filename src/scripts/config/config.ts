24539de077b2e1f673d1fb5827ae9b258fad2f1a
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2019-08-06 13:00:57 -0700

    use diff api

--- src/scripts/config/config.ts
+++ src/scripts/config/config.ts
@@ -2,11 +2,11 @@ import { Href } from './types';
 
 // to do - use different configs for dev and prod; and/or use env variable
 export const HOSTING = {
-  SUB_PATH: '/',
-  // SUB_PATH: '/myblog/hnpwa/',
+  // SUB_PATH: '/',
+  SUB_PATH: '/myblog/hnpwa/',
 };
 
-export const API = {
+export const API_SPARE = {
   BASE_URL: 'https://api.hnpwa.com/',
   VERSION: 'v0',
   topUrl: (page:number) => { return `/news/${page}.json`; },
@@ -16,6 +16,17 @@ export const API = {
   jobUrl: (page:number) => { return `/jobs/${page}.json`; },
   userUrl: (uid:string) => { return `/user/${uid}.json`; },
   itemUrl: (id:string) => { return `/item/${id}.json`; },
+};
+export const API = {
+  BASE_URL: 'https://node-hnapi.herokuapp.com/',
+  VERSION: '',
+  topUrl: (page:number) => { return `/news?page=${page}`; },
+  newUrl: (page:number) => { return `/newest?page=${page}`; },
+  showUrl: (page:number) => { return `/show?page=${page}`; },
+  askUrl: (page:number) => { return `/ask?page=${page}`; },
+  jobUrl: (page:number) => { return `/jobs?page=${page}`; },
+  userUrl: (uid:string) => { return `/user/${uid}`; },
+  itemUrl: (id:string) => { return `/item/${id}`; },
 };
 
 export const NAV_HREFS:Href[] = [
