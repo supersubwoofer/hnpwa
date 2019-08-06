e155b05e6a13a3e6e25e05716488f75a791c646a
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2019-08-06 14:50:48 -0700

    hide and show replies

--- src/scripts/components/hn-comment.ts
+++ src/scripts/components/hn-comment.ts
@@ -7,7 +7,7 @@ import { Item } from '../types';
 export class HnComment extends LitElement {
   @property() baseUrl: string = '/';
   @property() model: Item;
-  @property() isHideReply:boolean = false;
+  @property() isShownReply:boolean = true;
 
   static get styles() {
     return css`
@@ -21,16 +21,43 @@ export class HnComment extends LitElement {
       :host(.reply-comment) {
         padding: 1em 1em 0em 1em;
       }
+      .section-reply.hiden {
+        padding: 0 0 1em 0;
+      }
+      .section-reply.shown slot{
+        display: block;
+      }
+      .section-reply.hiden slot{
+        display: none;
+      }
+      .toggle-button {
+        cursor: pointer;
+      }
     `;
   }
 
   render() {
 
-    const toggleButton = (isShown: boolean) => {
-      return (isShown) ? html`<span class="to-show-reply">[-]</span>` :
-      html`<span class="to-hide-reply">[-]</span>`;
+    const toggleButton = (isShown: boolean, replyCount:number) => {
+      return (isShown) ?
+      html`<span class="toggle-button to-hide-reply" @click="${this.handleToggle}">[-]</span>` :
+      html`<span class="toggle-button to-show-reply" @click="${this.handleToggle}">
+      [+] ${replyCount} ${(replyCount > 1) ? 'replies hiden' : 'reply hiden'}
+      </span>`;
     };
-    const toggle = (this.model.comments.length > 0) ? toggleButton(this.isHideReply) : html``;
+
+    const toggleSection = (isShown: boolean, replyCount:number) => {
+      return html`
+      <section class="section-reply ${(isShown) ? 'shown' : 'hiden'}">
+      ${toggleButton(isShown, replyCount)}
+      <slot></slot>
+      </section>
+      `;
+    };
+
+    const toggleTemplate = (comments: Item[], isShown:boolean, replyCount:number) =>
+    (comments.length > 0) ?
+    toggleSection(isShown, replyCount) : html``;
 
     return html`
       <article>
@@ -38,8 +65,13 @@ export class HnComment extends LitElement {
       <a href="${this.baseUrl}user/${this.model.user}">${this.model.user}</a> ${this.model.time_ago}
       </header>
       <p>${until(unsafeHTML(this.model.content))}</p>
-      <section class="section-reply">${toggle}<slot></slot></section>
+      ${toggleTemplate(this.model.comments, this.isShownReply, this.model.comments.length)}
       </article>
     `;
+  }
+
+  handleToggle(e) {
+    console.log('toogle clicked');
+    this.isShownReply = !this.isShownReply;
   }
 }
