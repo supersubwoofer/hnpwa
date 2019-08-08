b4ee6c63fff6d3b5f7f18e58a18db74b3dd21be0
supersubwoofer@gmail.com <supersubwoofer@gmail.com>
2019-08-08 13:31:42 -0700

    apply workbox routing

--- src/scripts/global.d.ts
+++ src/scripts/global.d.ts
@@ -3,6 +3,7 @@ interface Window {
   registration: any;
   importScripts: any;
 }
+
 declare var self: Window;
 
 declare var workbox: import('workbox-sw');
--- src/scripts/worker.ts
+++ src/scripts/worker.ts
@@ -1,10 +1,35 @@
 self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
+const matchCb = ({ url, event }) => {
+  return (
+    url.pathname === '/news' ||
+    url.pathname === '/newest' ||
+    url.pathname === '/show' ||
+    url.pathname === '/ask' ||
+    url.pathname === '/jobs'
+  );
+};
+
+const handlerCb = ({ url, event, params }) => {
+  return fetch(event.request)
+  .then((response) => {
+    // console.log('service worker fetch to text');
+    return response.text();
+  })
+  .then((responseBody) => {
+    // console.log('service worker text to Response');
+    return new Response(`${responseBody}`);
+  });
+};
+
 if (workbox) {
   console.log('Yay! Workbox is loaded 🎉');
 
   // Precache
   workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+  // Request-Response Routing
+  workbox.routing.registerRoute(matchCb, handlerCb);
 
 } else {
   console.log('Boo! Workbox didn\'t load 😬');
